c4bf02df0c02b14f58ce8401dc4e531bedb17f81
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-14 19:29:10 +1000

    Adding Style with stylesheet

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
+import './Person.css';
 
 const person = (props) => {
 	return (
 		<Fragment>
-			<p onClick={props.click}>
-				I'm {props.name} and I am {props.age} years old!
-			</p>
-			<p>{props.children}</p>
-			<input type='text' onChange={props.changed} value={props.name}></input>
+			<div className='Person'>
+				<p onClick={props.click}>
+					I'm {props.name} and I am {props.age} years old!
+				</p>
+				<p>{props.children}</p>
+				<input type='text' onChange={props.changed} value={props.name}></input>
+			</div>
 		</Fragment>
 	);
 };
