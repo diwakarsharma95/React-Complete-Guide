1b94d9fa212766d16b1df91a6feab39312ae091d
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-14 19:12:54 +1000

    Adding Two way binding

--- src/App.js
+++ src/App.js
@@ -11,13 +11,22 @@ class App extends Component {
 		],
 	};
 
-	switchNameHandler = () => {
+	switchNameHandler = (newName) => {
 		//this.state.persons[0].name = 'Max Payne'; //DON'T DO THIS
 		this.setState({
 			persons: [
-				{ name: 'Max Payne', age: 28 },
+				{ name: newName, age: 28 },
 				{ name: 'Dave jonason', age: 26 },
 				{ name: 'John deCaprio', age: 30 },
+			],
+		});
+	};
+	nameChangeHandler = (event) => {
+		this.setState({
+			persons: [
+				{ name: 'Max', age: 28 },
+				{ name: event.target.value, age: 26 },
+				{ name: 'John', age: 30 },
 			],
 		});
 	};
@@ -25,12 +34,13 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<h1>Hi, I'm React App</h1>
-				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<button onClick={this.switchNameHandler.bind(this, 'Diwakar')}>Switch Name</button>
 				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
 				<Person
 					name={this.state.persons[1].name}
 					age={this.state.persons[1].age}
-					click={this.switchNameHandler}>
+					click={this.switchNameHandler.bind(this, 'Dinesh')}
+					changed={this.nameChangeHandler}>
 					My Hobbies: Racing
 				</Person>
 				<Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
