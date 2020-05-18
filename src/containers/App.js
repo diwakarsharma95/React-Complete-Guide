e809614155c57d8db34f87790c1a5d0c320cdd6b
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-18 21:02:11 +1000

    Component creation lifecycle in Action

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,10 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		console.log('[App.js] constructor');
+	}
 	state = {
 		persons: [
 			{ id: '1', name: 'Max', age: 28 },
@@ -13,6 +17,14 @@ class App extends Component {
 		otherState: 'some other value',
 		showPersons: false,
 	};
+
+	static getDerivedStateFromProps(props, state) {
+		console.log('[App.js] getDerivedStateFromProps', props);
+		return state;
+	}
+	componentDidMount() {
+		console.log('[App.js] componentDidMount');
+	}
 
 	nameChangeHandler = (event, id) => {
 		const personIndex = this.state.persons.findIndex((person) => {
@@ -43,6 +55,7 @@ class App extends Component {
 	};
 
 	render() {
+		console.log('[App.js] render');
 		let persons = null;
 		if (this.state.showPersons) {
 			persons = (
@@ -56,6 +69,7 @@ class App extends Component {
 		return (
 			<div className={classes.App}>
 				<Cockpit
+					title={this.props.appTitle}
 					showPersons={this.state.showPersons}
 					persons={this.state.persons}
 					clicked={this.togglePersonHandler}></Cockpit>
